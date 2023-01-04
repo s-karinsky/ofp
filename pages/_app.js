9c5e51adb52acfe4310e55994d76fa84822db1fa
Sergey Karinsky <karinsky.dev@gmail.com>
2023-01-04 19:45:34 +0700

    Header output

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import { Provider } from 'react-redux'
 import { store } from '@store'
+import Header from '@components/Header'
+import { PopupOverlay } from '@components/Popup'
 import '@styles/styles.scss'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <PopupOverlay />
+      <Header />
       <div className="container">
         <Component {...pageProps} />
       </div>
