711efd493ce250c0c2cd0569c9baffeb931f129b
Sergey Karinsky <karinsky.dev@gmail.com>
2023-02-04 16:58:15 +0700

    Fix: create user before sending mail

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -10,6 +10,14 @@ async function createUser(req, res) {
     try {
         const confirmationCode = (Math.random() + 1).toString(36).substring(2)
 
+        await User.create({
+            email,
+            password,
+            name: `${surname} ${name}`,
+            phone,
+            confirmationCode
+        })
+
         await sendMail({
             from: `Ортофотоплан <${process.env.SMTP_USER}>`,
             to: email,
@@ -18,14 +26,6 @@ async function createUser(req, res) {
                 Этот e-mail был указан для регистрации на сайте ортофотоплан<br>
                 Для подтверждения перейдите по ссылке <a href="${process.env.BASE_URL}/api/confirm?email=${email}&code=${confirmationCode}" target="_blank">${process.env.BASE_URL}/api/confirm?email=${email}&code=${confirmationCode}</a>
             `
-        })
-
-        await User.create({
-            email,
-            password,
-            name: `${surname} ${name}`,
-            phone,
-            confirmationCode
         })
 
         res.status(201).json({ message: 'User created' })
